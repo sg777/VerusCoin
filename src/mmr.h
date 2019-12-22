8979efe6cbb4ee5cc03c85b6f8efd3a307b3f846
miketout <23489320+miketout@users.noreply.github.com>
2019-12-22 00:53:16 -0800

    build break for gcc8

--- src/mmr.h
+++ src/mmr.h
@@ -220,7 +220,7 @@ public:
         {
             return nodes[idx >> CHUNK_SHIFT][idx & chunkMask()];
         }
-        NODE_TYPE();
+        return NODE_TYPE();
     }
 
     void push_back(NODE_TYPE node)
