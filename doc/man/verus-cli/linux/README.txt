096bc181c37c4aaa44af6b3155eb2d4d161b269e
Asher Dawes <asher.dawes@gmail.com>
2020-06-05 03:16:12 -0700

    bump version

--- doc/man/verus-cli/linux/README.txt
+++ doc/man/verus-cli/linux/README.txt
@@ -1,4 +1,4 @@
-VerusCoin Command Line Tools v0.6.5-dev-312
+VerusCoin Command Line Tools v0.6.5-dev-33
 
 Contents:
 verusd - VerusCoin daemon
--- src/version.h
+++ src/version.h
@@ -35,6 +35,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 static const int NO_BLOOM_VERSION = 170004;
 
 #define KOMODO_VERSION "0.2.1"
-#define VERUS_VERSION "0.6.5-dev-312"
+#define VERUS_VERSION "0.6.5-dev-33"
 
 #endif // BITCOIN_VERSION_H
