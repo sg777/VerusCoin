3acf759015668baa9fc7a2fc622c5200a51242bd
miketout <michaeltoutonghi@gmail.com>
2020-07-13 00:17:53 -0700

    Add fees for CONVERT transfers

--- src/pbaas/reserves.cpp
+++ src/pbaas/reserves.cpp
@@ -1563,7 +1563,7 @@ bool CReserveTransactionDescriptor::AddReserveTransferImportOutputs(const uint16
                             AddReserveInput(curTransfer.currencyID, curTransfer.nValue + curTransfer.nFees);
                         }
                     }
-                    if (!(curTransfer.flags & (curTransfer.PRECONVERT | curTransfer.CONVERT)))
+                    if (!(curTransfer.flags & (curTransfer.PRECONVERT)))
                     {
                         transferFees.valueMap[curTransfer.currencyID] += curTransfer.nFees;
                     }
