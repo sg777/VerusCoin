4bf52ab6a9f3a7caf5320bcb3a66e9d4965134c7
miketout <michaeltoutonghi@gmail.com>
2020-09-14 11:29:09 -0700

    Fix redundant fee check

--- src/pbaas/reserves.cpp
+++ src/pbaas/reserves.cpp
@@ -2224,12 +2224,11 @@ bool CReserveTransactionDescriptor::AddReserveTransferImportOutputs(const uint16
     }
     CCrossChainExport ccx(systemDestID, numTransfers, ReserveInputs, transferFees);
     //printf("ReserveInputs: %s\nReserveOutputs: %s\nReserveInputs - ReserveOutputs: %s\n", ReserveInputs.ToUniValue().write(1,2).c_str(), ReserveOutputs.ToUniValue().write(1,2).c_str(), (ReserveInputs - ReserveOutputs).ToUniValue().write(1,2).c_str());
-    if (ReserveInputs - ReserveOutputs < ccx.CalculateImportFee())
+    if ((ReserveInputs - ReserveOutputs).HasNegative())
     {
-        printf("%s: Too much fee taken by export, ReserveInputs: %s\nReserveOutputs: %s\nccx.CalculateImportFee(): %s\nccx: %s\n", __func__,
+        printf("%s: Too much fee taken by export, ReserveInputs: %s\nReserveOutputs: %s\nccx: %s\n", __func__,
                 ReserveInputs.ToUniValue().write(1,2).c_str(), 
                 ReserveOutputs.ToUniValue().write(1,2).c_str(), 
-                ccx.CalculateImportFee().ToUniValue().write(1,2).c_str(), 
                 ccx.ToUniValue().write(1,2).c_str());
         LogPrintf("%s: Too much fee taken by export\n", __func__);
         return false;
