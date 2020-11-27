e7465a7bc39e292cb2772bc3fd969d5586d1b013
miketout <michaeltoutonghi@gmail.com>
2020-11-27 13:04:18 -0800

    Add one boolean parameter to client for getcurrencybalance

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -401,6 +401,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getbalance", 1 },
     { "getbalance", 2 },
     { "getcurrencybalance", 1},
+    { "getcurrencybalance", 2},
     { "getblockhash", 0 },
     { "move", 2 },
     { "move", 3 },
