2095c1135d59f2719a4366be748c2e34abca8557
miketout <michaeltoutonghi@gmail.com>
2021-05-03 01:16:16 -0700

    Build fix

--- src/pbaas/pbaas.cpp
+++ src/pbaas/pbaas.cpp
@@ -1747,7 +1747,7 @@ bool CConnectedChains::CreateLatestImports(const CCurrencyDefinition &sourceSyst
 
         if (isRefundingSeparateChain)
         {
-            printf("%s: processing refund from chain %s\n", __func__, refundingPBaaSChain.name);
+            printf("%s: processing refund from chain %s\n", __func__, refundingPBaaSChain.name.c_str());
         }
 
         // get reserve deposits for destination currency of export. these will be available whether the source is same chain
