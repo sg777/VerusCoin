6bf613454aba146d9bccde63af2c402054ade934
miketout <michaeltoutonghi@gmail.com>
2021-05-13 13:34:32 -0700

    Burn fractional with conversions fix

--- src/pbaas/pbaas.cpp
+++ src/pbaas/pbaas.cpp
@@ -2034,12 +2034,20 @@ bool CConnectedChains::CreateLatestImports(const CCurrencyDefinition &sourceSyst
         newNotarization.prevNotarization = CUTXORef(lastNotarizationOut.txIn.prevout.hash, lastNotarizationOut.txIn.prevout.n);
 
         CCurrencyValueMap incomingCurrency = importedCurrency + gatewayDepositsUsed;
-        if (newNotarization.currencyState.primaryCurrencyOut)
+        CCurrencyValueMap newLocalReserveDeposits;
+        CCurrencyValueMap newLocalDepositsRequired;
+        if (newNotarization.currencyState.primaryCurrencyOut > 0)
         {
             incomingCurrency.valueMap[destCurID] += newNotarization.currencyState.primaryCurrencyOut;
         }
-        CCurrencyValueMap newLocalReserveDeposits = incomingCurrency.SubtractToZero(spentCurrencyOut);
-        CCurrencyValueMap newLocalDepositsRequired = ((incomingCurrency - spentCurrencyOut) - newLocalReserveDeposits).CanonicalMap() * -1;
+        newLocalReserveDeposits = incomingCurrency.SubtractToZero(spentCurrencyOut);
+        newLocalDepositsRequired += (((incomingCurrency - spentCurrencyOut) - newLocalReserveDeposits).CanonicalMap() * -1);
+        if (newNotarization.currencyState.primaryCurrencyOut < 0)
+        {
+            // we need to come up with this currency, as it will be burned
+            incomingCurrency.valueMap[destCurID] += newNotarization.currencyState.primaryCurrencyOut;
+            newLocalDepositsRequired.valueMap[destCurID] -= newNotarization.currencyState.primaryCurrencyOut;
+        }
 
         /* printf("%s: newNotarization:\n%s\n", __func__, newNotarization.ToUniValue().write(1,2).c_str());
         printf("%s: ccx.totalAmounts: %s\ngatewayDepositsUsed: %s\nimportedCurrency: %s\nspentCurrencyOut: %s\n",
