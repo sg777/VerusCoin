d0f419c001b14b05f37cf88fb0a03dc33550f62f
miketout <michaeltoutonghi@gmail.com>
2021-06-14 21:57:35 -0700

    Default to private change only when specified with "-privatechange"

--- src/init.cpp
+++ src/init.cpp
@@ -1265,7 +1265,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         {
         }
     }
-    VERUS_PRIVATECHANGE = GetBoolArg("-privatechange", !GetArg("-defaultzaddr", VERUS_DEFAULT_ZADDR).empty());
+    VERUS_PRIVATECHANGE = GetBoolArg("-privatechange", false);
 
     // Sanity check
     if (!InitSanityCheck())
