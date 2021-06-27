fd645e2598bb9980be432351c1f39f51f093e4b9
miketout <michaeltoutonghi@gmail.com>
2021-06-26 17:37:13 -0700

    TESTING ONLY - revert behavior to before instaban excessively bad peers fix

--- src/main.cpp
+++ src/main.cpp
@@ -8427,7 +8427,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 {
                     if (nDoS > 0 && futureblock == 0)
                     {
-                        Misbehaving(pfrom->GetId(), nDoS);
+                        // TODO: HARDENING - DEBUG reduction of ban score as per pre-Verus as honeypot to
+                        // catch specific issues
+                        Misbehaving(pfrom->GetId(), 1);
+                        //Misbehaving(pfrom->GetId(), nDoS);
                     }
                     return error("invalid header received");
                 }
