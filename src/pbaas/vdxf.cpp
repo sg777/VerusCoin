06424bb4a5e6dff2679e122742299b64ae710dc6
miketout <michaeltoutonghi@gmail.com>
2023-03-01 20:29:48 -0800

    Sync fix

--- src/pbaas/vdxf.cpp
+++ src/pbaas/vdxf.cpp
@@ -53,7 +53,9 @@ std::string TrimTrailing(const std::string &Name, unsigned char ch)
 std::string TrimSpaces(const std::string &Name, bool removeDuals)
 {
     std::string nameCopy = Name;
-    std::string noDuals = "\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200C\u200D\u202F\u205F\u3000";
+    std::string noDuals = removeDuals ?
+        "\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200C\u200D\u202F\u205F\u3000" :
+        " ";
     std::vector<int> allDuals;
     std::vector<int> toRemove;
     for (int i = 0; i < nameCopy.size(); i++)
