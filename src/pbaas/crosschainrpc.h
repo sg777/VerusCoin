e325f9b612871d3c38d1fe171ee4abd007afa979
miketout <michaeltoutonghi@gmail.com>
2023-04-14 20:57:08 -0700

    Cleanup

--- src/pbaas/crosschainrpc.h
+++ src/pbaas/crosschainrpc.h
@@ -1276,8 +1276,6 @@ public:
 class CIdentitySignature
 {
 public:
-    // TODO HARDENING - move all instances post PBaaS to
-    // VERSION_ETHBRIDGE
     enum EVersions {
         VERSION_INVALID = 0,
         VERSION_VERUSID = 1,
