f8f66e41a3f44e5c1e94f63b322d8bf063377cae
miketout <michaeltoutonghi@gmail.com>
2023-04-18 23:24:42 -0700

    Set mainnet PBaaS activation to 2546600

--- src/init.cpp
+++ src/init.cpp
@@ -1252,6 +1252,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
             CConstVerusSolutionVector::activationHeight.SetActivationHeight(CActivationHeight::SOLUTION_VERUSV5, 1053660);
             CConstVerusSolutionVector::activationHeight.SetActivationHeight(CActivationHeight::SOLUTION_VERUSV5_1, 1053660);
             CConstVerusSolutionVector::activationHeight.SetActivationHeight(CActivationHeight::SOLUTION_VERUSV6, 1796400);
+            CConstVerusSolutionVector::activationHeight.SetActivationHeight(CActivationHeight::SOLUTION_VERUSV7, 2546600);
         }
         else if (IsVerusActive())
         {
